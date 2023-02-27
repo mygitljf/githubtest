23a4d481ef63e0675dd081c087de49053f8c18ad
ljf <2410316423@qq.com>
2023-02-27 09:59:11 +0800

    add

---
+++ cmake-build-debug/CMakeFiles/compiler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler.dir/hello.cpp.o"
+  "CMakeFiles/compiler.dir/hello.cpp.o.d"
+  "compiler"
+  "compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/compiler.dir/hello.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/compiler.dir/hello.cpp.o: \
+ /home/tom/桌面/gittext/githubtest/hello.cpp /usr/include/stdc-predef.h \
+ /home/tom/桌面/gittext/githubtest/hello.h
